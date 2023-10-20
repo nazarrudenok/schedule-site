af3212400bbbc7698fd4ee9f931441c6b00ffbbf
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-10-20 23:18:27 +0300

    Add files via upload

--- js/links-module.js
+++ js/links-module.js
@@ -1,27 +1,39 @@
-import data from "../jsons/links.json" assert {type: 'json'};
+const jsonURL = "../jsons/links.json";
 
-var container = document.querySelector('.container');
+fetch(jsonURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    var container = document.querySelector(".container");
 
-for (var i in data) {
-    var name = i;
-    var link = data[i]['url'];
-    var type = data[i]['type'];
+    for (var i in data) {
+      var name = i;
+      var link = data[i]["url"];
+      var type = data[i]["type"];
 
-    var elContDiv = document.createElement('div');
-    elContDiv.className = 'el-cont';
+      var elContDiv = document.createElement("div");
+      elContDiv.className = "el-cont";
 
-    var nameParagraph = document.createElement('p');
-    nameParagraph.className = 'el-item';
-    nameParagraph.textContent = name;
+      var nameParagraph = document.createElement("p");
+      nameParagraph.className = "el-item";
+      nameParagraph.textContent = name;
 
-    var typeLink = document.createElement('a');
-    typeLink.className = 'type';
-    typeLink.href = link;
-    typeLink.target = '_blank';
-    typeLink.textContent = type;
+      var typeLink = document.createElement("a");
+      typeLink.className = "type";
+      typeLink.href = link;
+      typeLink.target = "_blank";
+      typeLink.textContent = type;
 
-    elContDiv.appendChild(nameParagraph);
-    elContDiv.appendChild(typeLink);
+      elContDiv.appendChild(nameParagraph);
+      elContDiv.appendChild(typeLink);
 
-    container.appendChild(elContDiv);
-}
+      container.appendChild(elContDiv);
+    }
+  })
+  .catch(error => {
+    console.error("Error loading JSON:", error);
+  });
--- js/module2.js
+++ js/module2.js
@@ -1,10 +1,15 @@
-import data from "../jsons/group2.json" assert {type: 'json'};
-// import schedule from "../jsons/hours.json" assert {type: 'json'};
-// console.log(schedule)
+const jsonURL = "../jsons/group2.json";
 
-
-for (var i in data) {
-    if (data.hasOwnProperty(i)) {
+fetch(jsonURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    for (var i in data) {
+      if (data.hasOwnProperty(i)) {
         var dayCont = document.createElement('div');
         dayCont.classList.add('day-cont');
 
@@ -19,25 +24,26 @@ for (var i in data) {
         lessonContainer.classList.add('lesson-container');
 
         for (var j = 0; j < data[i].length; j++) {
-            var lessonItemContainer = document.createElement('div');
-            lessonItemContainer.classList.add('lessons-container');
+          var lessonItemContainer = document.createElement('div');
+          lessonItemContainer.classList.add('lessons-container');
 
-            var lessonItemNumber = document.createElement('p');
-            lessonItemNumber.textContent = (j + 1);
-            lessonItemNumber.classList.add('lesson-item-number');
-            lessonItemContainer.appendChild(lessonItemNumber);
+          var lessonItemNumber = document.createElement('p');
+          lessonItemNumber.textContent = (j + 1);
+          lessonItemNumber.classList.add('lesson-item-number');
+          lessonItemContainer.appendChild(lessonItemNumber);
 
-            var lessonItem = document.createElement('p');
-            lessonItem.textContent = data[i][j];
-            lessonItem.classList.add('lesson-item');
-            lessonItemContainer.appendChild(lessonItem);
+          var lessonItem = document.createElement('p');
+          lessonItem.textContent = data[i][j];
+          lessonItem.classList.add('lesson-item');
+          lessonItemContainer.appendChild(lessonItem);
 
-            lessonContainer.appendChild(lessonItemContainer);
+          lessonContainer.appendChild(lessonItemContainer);
         }
 
         dayCont.appendChild(lessonContainer);
+      }
     }
-}
-
-var num1 = document.getElementsByClassName('lesson-item-number')[35];
-num1.remove()
+  })
+  .catch(error => {
+    console.error("Error loading JSON:", error);
+  });
